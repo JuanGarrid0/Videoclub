3ab29a76b61ee31a24f44e078e6394b5ec3f396d
JuanGarrid0 <114677191+JuanGarrid0@users.noreply.github.com>
2024-11-11 00:05:52 +0100

    añadir htmls y probar primer url

---
+++ videoclub/movies/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+from movies import views
+
+urlpatterns = [
+    path('', views.index, name ="index"),
+    path('director', views.director, name="director"),
+    path('genero', views.genero, name="genero"),
+    path('lista', views.lista, name="lista"),
+
+]
--- videoclub/movies/views.py
+++ videoclub/movies/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+movies=["El señor de lls anillos",
+        "Juego de Tronos",
+        "Pulgarcito",
+        "Harry Potter",
+        "Karate Kid" ]
+
+def index(request):
+    return render(request, "movies/index.html", { "items" : movies})
+
+def director():
+    return
+
+def genero():
+    return
+
+def lista():
+    return
