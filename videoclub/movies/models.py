700d6a89513f63f261d8c2abf6a77dbddca7f88b
JuanGarrid0 <114677191+JuanGarrid0@users.noreply.github.com>
2024-11-12 01:27:11 +0100

    modelos, admin y inicio pagina directores

--- videoclub/movies/models.py
+++ videoclub/movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Director(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=30)
+
+class Movie (models.Model):
+    movies= models.TextChoices("Pelicula", "Serie")
+    genero = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
+    directores = models.ManyToManyField(Director)
+    tipoMovie = models.CharField(choices=movies, max_length=20)
+
--- videoclub/movies/views.py
+++ videoclub/movies/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Director, Movie
+
 # Create your views here.
 
 movies=["El señor de lls anillos",
@@ -8,7 +10,7 @@ movies=["El señor de lls anillos",
         "Pulgarcito",
         "Harry Potter",
         "Karate Kid" ]
-directores=["directores"]
+directores=[]
 
 generos=["generos"]
 
@@ -18,6 +20,9 @@ def index(request):
     return render(request, "movies/index.html", { "items" : movies})
 
 def director(request):
+    di=Director.objects.all()
+    for item in di:
+        directores.append(item.nombre +" "+item.apellido)
     return render(request, "movies/director.html", {"director": directores} )
 
 def genero(request):
